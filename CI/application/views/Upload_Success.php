55c170c94e41fa0d2c2880b39c423850c0387e0e
Swatantra Gupta <SwatantraGupta-1402@users.noreply.github.com>
2017-01-11 02:43:21 -0800

    Update Upload_Success.php

--- CI/application/views/Upload_Success.php
+++ CI/application/views/Upload_Success.php
@@ -4,12 +4,12 @@
 	<title>Code Igniter Upload Form</title>
 </head>
 <body>
-<h3>Your File has been successfully uploaded.</h3>
+<h3 style="color:green;">Your File has been successfully uploaded.</h3>
 <ul>
 	<?php foreach ($upload_data as $item => $value) ?>
-		<li><?php echo $item; ?>:<?php echo $value; ?></li>
+		<li><?php echo $item; ?>:&nbsp;&nbsp;&nbsp;<?php echo $value; ?></li>
 	<?phpendforeach; ?>
 </ul>
-<p><?php echo anchor('upload','Upload Another File!'); ?></p>
+<p><?php echo anchor('upload','<b>Upload Another File!</b>'); ?></p>
 </body>
 </html>
